f76df75567a7efbfc86088f2f645c8d8c31d3d4c
tarun <tarunreddy.b@enterpi.com>
2026-03-09 11:07:02 +0530

    home page ui done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, Layers } from "lucide-react";
+import { ArrowRight, Clock, Layers } from "lucide-react";
 import type { Route } from "./+types/home";
 
 import Navbar from "components/Navbar";
@@ -36,16 +36,50 @@ export default function Home() {
           </Button>
         </div>
         <div id="upload" className="upload-shell">
-          <div className="grid-overlay">
-            <div className="upload-card">
-              <div className="upload-head">
-                <div className="upload-icon">
-                  <Layers className="icon" />
-                </div>
-                <h3>Upload your floor plan</h3>
-                <p>Supports JPG, PNG,formats upto 10MB</p>
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
               </div>
-              <p>Upload Images</p>
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG,formats upto 10MB</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all at one place.</p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group"> 
+              <div className="preview">
+                <img src="" alt="" />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12}/>
+                    <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                    <span>By Ram</span>
+                  </div>
+                </div>
+<div className="arrow">
+  <ArrowRight size={18}/>
+</div>
+              </div>
             </div>
           </div>
         </div>
